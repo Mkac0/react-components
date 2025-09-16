6b2ee23d641b59123a70026a0b8fe3ff9b40aa78
melabreu <melissa.abreu84@gmail.com>
2025-09-16 17:37:02 -0400

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -2,52 +2,56 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-const weatherForecasts = [
-  {
-    day: 'Mon',
-    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/day.svg',
-    imgAlt: 'sun icon',
-    conditions: 'sunny',
-    time: 'Morning',
-  },
-  {
-    day: 'Tue',
-    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/night.svg',
-    imgAlt: 'moon icon',
-    conditions: 'clear',
-    time: 'Night',
-  },
-  {
-    day: 'Wed',
-    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/stormy.svg',
-    imgAlt: 'clouds with lightning icon',
-    conditions: 'stormy',
-    time: 'All Day',
-  },
-  {
-    day: 'Thu',
-    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-day.svg',
-    imgAlt: 'sun overcast by clouds icon',
-    conditions: 'overcast',
-    time: 'Evening',
-  },
-  {
-    day: 'Fri',
-    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-night.svg',
-    imgAlt: 'moon overcast by clouds icon',
-    conditions: 'cloudy',
-    time: 'Night',
-  },
-];
+import WeatherForecast from './components/WeatherForecast'
 
 
 const App = () => {
+  const weatherForecasts = [
+    {
+      day: 'Mon',
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/day.svg',
+      imgAlt: 'sun icon',
+      conditions: 'sunny',
+      time: 'Morning',
+    },
+    {
+      day: 'Tue',
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/night.svg',
+      imgAlt: 'moon icon',
+      conditions: 'clear',
+      time: 'Night',
+    },
+    {
+      day: 'Wed',
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/stormy.svg',
+      imgAlt: 'clouds with lightning icon',
+      conditions: 'stormy',
+      time: 'All Day',
+    },
+    {
+      day: 'Thu',
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-day.svg',
+      imgAlt: 'sun overcast by clouds icon',
+      conditions: 'overcast',
+      time: 'Evening',
+    },
+    {
+      day: 'Fri',
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-night.svg',
+      imgAlt: 'moon overcast by clouds icon',
+      conditions: 'cloudy',
+      time: 'Night',
+    },
+  ];
   return (
     <>
       <h1>Local Weather</h1>
       <section>
-        // Weather data here
+        {weatherForecasts.map((weather) => (
+          <WeatherForecast key={weatherForecasts}
+          {...weather}
+          />
+        ))}
       </section>
     </>
   );
---
+++ src/components/WeatherForecast.jsx
@@ -0,0 +1,14 @@
+import './WeatherForecast.css'
+
+const WeatherForecast = ({ day, conditions, time, img, imgAlt }) => {
+    return (
+        <div className="weather">
+        <h2>{day}</h2>
+        <img src={img} alt={imgAlt} />
+        <p><span>Conditions: </span>{conditions}</p>
+        <p><span>Time: </span>{time}</p>
+    </div>
+    );
+}
+
+export default WeatherForecast;
